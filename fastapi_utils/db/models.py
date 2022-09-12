eee913224bd6c08ec7084e8b892b29d30c55af67
Andrey Shaikin <kiwibon@yandex.ru>
2022-09-12 19:05:33 +0300

    отдельно meta-класс

--- fastapi_utils/db/models.py
+++ fastapi_utils/db/models.py
@@ -9,14 +9,18 @@ class BaseModelQueryset(ormar.queryset.QuerySet):
     pass
 
 
+class BaseModelMeta(ormar.Model.Meta):
+    abstract = True
+    database = settings.db
+    metadata = settings.db_meta
+    queryset_class = BaseModelQueryset
+    orders_by = ["created_at", "id"]
+
+
 class BaseModel(ormar.Model):
     uuid: int = ormar.UUID(default=uuid.uuid4)
     id: int = ormar.Integer(primary_key=True, unique=True, autoincrement=True)
     created_at: datetime = ormar.DateTime(timezone=True, default=datetime.now, nullable=True)
 
-    class Meta(ormar.Model.Meta):
-        abstract = True
-        database = settings.db
-        metadata = settings.db_meta
-        queryset_class = BaseModelQueryset
-        orders_by = ["created_at", "id"]
+    class Meta(BaseModelMeta):
+        pass
